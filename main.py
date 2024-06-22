c329ffa9d16304d57aa6f2f88a0310f354580a79
Fahim <ec22827@qmul.ac.uk>
2024-06-22 19:00:12 +0100

    1.01

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from config import API_ID, API_HASH
-from modules import misc.py
+from modules import misc
 app = Client("my_account", api_id=API_ID, api_hash=API_HASH)
 
 
-@app.on_message(filters.private)
-async def hello(client, message):
-    await message.reply("Hello from Pyrogram!")
+@app.on_message(filters.command(["help"]), filters.me)
+async def my_handler(client, message):
+    await message.edit("Pyrogram")
 
 
 if __name__ == '__main__':
